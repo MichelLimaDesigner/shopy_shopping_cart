da915a452bf6837ebf2ea906094644d561e2a0d6
michellimadesigner <michellimadesigner@outlook.com>
2024-09-26 02:40:42 -0300

    feat: finish cart logic

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ const item1 = await Item.createItem("HotWheels", 15.99, 3);
 const item2 = await Item.createItem("MaxSteel", 59.99, 1);
 const item3 = await Item.createItem("Super Nintendo", 199.99, 1);
 
-Cart.addItem(cart, item1);
-Cart.addItem(cart, item2);
-Cart.addItem(wishList, item3);
+await Cart.addItem(cart, item1);
+await Cart.addItem(cart, item2);
+await Cart.addItem(wishList, item3);
 
-console.log("💵 Your total in cart is:");
+await Cart.removeItem(cart, item1);
+// await Cart.deleteItem(cart, item1.name);
+
+await Cart.displayCart(cart);
+
+console.log("\n💵 Total:");
 await Cart.calculateTotal(cart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -2,13 +2,44 @@ async function addItem(userCart, item) {
   userCart.push(item);
 }
 
-async function deleteItem(userCart, name) {}
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+  if (index !== -1) userCart.splice(index, 1);
+}
 
-async function removeItem(userCart, index) {}
+async function removeItem(userCart, item) {
+  const indexFound = userCart.findIndex((e) => e.name === item.name);
+
+  if (indexFound === -1) {
+    console.log("🚨 Item not found!");
+    return;
+  }
+
+  if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+    console.log(`\n ✅ Item removed | ${item.name}. ${item.quantity}x`);
+    return;
+  }
+
+  if (userCart[indexFound].quantity === 1) {
+    userCart.splice(indexFound, 1);
+  }
+}
 
 async function calculateTotal(userCart) {
   const result = userCart.reduce((total, item) => total + item.subTotal(), 0);
   console.log(`R$ ${result.toFixed(2)}`);
 }
 
-export { addItem, deleteItem, removeItem, calculateTotal };
+async function displayCart(userCart) {
+  console.log("\n Shopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      }x | Subtotal: ${item.subTotal()}`
+    );
+  });
+}
+
+export { addItem, deleteItem, removeItem, calculateTotal, displayCart };
